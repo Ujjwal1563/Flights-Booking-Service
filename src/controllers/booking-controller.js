d30df554e2aa377900f1b73359329d60ef5a551f
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-02-04 18:38:06 +0530

    Started Booking impl

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,3 +1,22 @@
-module.exports={
-    
+const { StatusCodes } = require("http-status-codes");
+const { BookingService } = require("../services");
+const ErrorResponse = require("../utils/common/error-response");
+const SuccessResponse = require("../utils/common/success-response");
+
+async function createBooking(req, res) {
+  try {
+    const response = await BookingService.createBooking({
+      flightId: req.body.flightId,
+      userId: req.body.userId,
+      noOfSeats: req.body.noOfSeats,
+    });
+    SuccessResponse.data = response;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
 }
+module.exports = {
+  createBooking,
+};
